70ddce5f49dec497579f8f98e0b2590b098ccfc1
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-11-14 10:13:47 -0500

    e

--- project/js/main.js
+++ project/js/main.js
@@ -2,3 +2,356 @@ import '../css/style.css';
 import {menu, name} from "./menu.js"
 
 console.log(menu, name);
+
+const char = [
+  {
+    charName: "Mika Kagehira",
+    cardName: "[In the Shadow] Mika Kagehira",
+    dateAvailable: "6/18/22 - 7/2/22",
+    agency: "COSMIC PRODUCTION",
+    units: "Valkyrie",
+    img: "./public/mikakagehira.jpg"
+  },
+  {
+    charName: "Tatsumi Kazehaya",
+    cardName: "[Silent Prayer] Tatsumi Kazehaya",
+    dateAvailable: "7/2/22 - 7/17/22",
+    agency: "STARMAKER PRODUCTION",
+    units: "ALKALOID",
+    img: ""
+  },
+  {
+    charName: "Ibara Saegusa",
+    cardName: "[Disguised Commander] Ibara Saegusa",
+    dateAvailable: "7/17/22 - 8/1/22",
+    agency: "COSMIC PRODUCTION",
+    units: "Eden, Adam",
+    img: ""
+  },
+  {
+    charName: "Koga Ogami",
+    cardName: "[Glittering Rock Wolf] Koga Ogami",
+    dateAvailable: "8/1/22 - 8/15/22",
+    agency: "Rhythm Link",
+    units: "UNDEAD",
+    img: ""
+  },
+  {
+    charName: "Izumi Sena",
+    cardName: "[Fairy of Morning Mist] Izumi Sena",
+    dateAvailable: "8/15/22 - 8/29/22",
+    agency: "Rhythm Link",
+    units: "Knights",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  },
+  {
+    charName: "",
+    cardName: "",
+    dateAvailable: "",
+    units: "",
+    img: ""
+  }
+];
+
+char.forEach(el => {});
