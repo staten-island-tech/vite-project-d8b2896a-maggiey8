dfa49ffae4f50877f4e28fbf577ae566aefc38e2
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-11-22 09:53:29 -0500

    werwerrwg

--- project/js/dom.js
+++ project/js/dom.js
@@ -10,13 +10,42 @@ const DOMSelectors = {
   card: document.querySelectorAll(".gallery")
 }
 
+function starproFilter() {
+    const starpro = char.filter((el) => el.agency.includes("STARMAKER PRODUCTION"))
+    starpro.forEach((el) => newCard(el))
+}
+
+function cosproFilter() {
+    let keepCard = char.filter((el) => el.agency.includes("COSMIC PRODUCTION")).target.parentElement
+    console.log(keepCard)
+
+}
+
+function rhythlinFilter() {
+    char.filter((el) => el.agency.includes("Rhythm Link"))
+}
+
+function newdiFilter() {
+    char.filter((el) => el.agency.includes("New Dimension"))
+}
+
 DOMSelectors.starpro.addEventListener("click", function(event) {
     event.preventDefault()
     starproFilter();
 }
 )
 
-function starproFilter() {
-    const starpro = char.filter((el) => el.agency.includes("STARMAKER PRODUCTION"))
-    console.log(starpro)
-}
+DOMSelectors.cospro.addEventListener("click", function(event) {
+    event.preventDefault()
+    cosproFilter();
+})
+
+DOMSelectors.rhythlin.addEventListener("click", function(event) {
+    event.preventDefault()
+    rhythlinFilter();
+})
+
+DOMSelectors.newdi.addEventListener("click", function(event) {
+    event.preventDefault()
+    newdiFilter();
+})
